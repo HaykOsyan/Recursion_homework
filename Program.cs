39e6c1979a601662e83e56238291be11f2b0a9fa
«HaykOsyan <ohayk@mail.ru>
2024-07-10 19:38:12 +0400

    first task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    Console.WriteLine("type number M");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("type number N");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    if (numberM < numberN - 1)
+    {
+        NatNumbers(numberM, numberN);
+    }
+    else
+    {
+        Console.WriteLine("There are no nat numbers between M and N");
+    }
+
+}
+int NatNumbers(int M, int N)
+{
+    int nextNumber = M + 1;
+    if (nextNumber + 1 > N)
+    {
+        return nextNumber;
+    }
+    System.Console.WriteLine(nextNumber + " ");
+    return NatNumbers(nextNumber, N);
+}
+Main();
